313688b37b197d7f42e1e2e840f3c40b73ac78de
Artur Khalikov <akhalikoff@gmail.com>
2015-08-27 15:55:22 +0300

    thread interruption demo

---
+++ src/main/java/basics/ThreadStop.java
@@ -0,0 +1,69 @@
+package basics;
+
+import utils.ThreadHelper;
+
+/**
+ * Example demonstrates how a thread can be interrupted
+ *
+ * @author ahalikov
+ */
+public class ThreadStop {
+
+    public static void main(String[] args) {
+        ThreadHelper.clog("started");
+
+        //interruptedExceptionExample();
+        interruptedStatusExample(true);
+
+        ThreadHelper.clog("finished");
+    }
+
+    static void interruptedExceptionExample() {
+        Thread thread = new Thread(
+                () -> {
+                    ThreadHelper.clog("started");
+                    try {
+                        Thread.sleep(1000 * 10);
+
+                        ThreadHelper.clog("finished");
+                    } catch (InterruptedException ie) {
+                        ThreadHelper.clog("interrupted, isInterrupted="
+                                + Thread.currentThread().isInterrupted());
+                    }
+                }
+        );
+        thread.start();
+
+        ThreadHelper.sleepSeconds(2);
+
+        thread.interrupt();
+
+    }
+
+    static void interruptedStatusExample(boolean clearInterruptedStatus) {
+        Thread thread = new Thread(
+                () -> {
+                    ThreadHelper.clog("started");
+                    while (true) {
+                        if (clearInterruptedStatus) {
+                            if (Thread.interrupted()) {
+                                ThreadHelper.clog("interrupted, isInterrupted="
+                                        + Thread.currentThread().isInterrupted() + " => cleared");
+                            }
+                        } else {
+                            if (Thread.currentThread().isInterrupted()) {
+                                ThreadHelper.clog("interrupted, isInterrupted="
+                                        + Thread.currentThread().isInterrupted());
+                                return;
+                            }
+                        }
+                    }
+                }
+        );
+        thread.start();
+
+        ThreadHelper.sleepSeconds(2);
+
+        thread.interrupt();
+    }
+}
