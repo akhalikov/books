9442d72ea7d4093983ae24026b1e82d8655c78a6
Artur Khalikov <akhalikoff@gmail.com>
2015-10-12 16:55:49 +0300

    Create ReentrantReadWriteLock2.java

---
+++ jcp/src/main/java/basics/locks/ReentrantReadWriteLock2.java
@@ -0,0 +1,65 @@
+package basics.locks;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Write-reentrant read-write lock.
+ * Write reentrance is granted only if the thread has already write access.
+ *
+ * {@See http://tutorials.jenkov.com/java-concurrency/read-write-locks.html}
+ */
+public class ReentrantReadWriteLock2 implements ReadWriteLock {
+
+    private Map<Thread, Integer> readingThreads = new HashMap();
+
+    private int writeAccesses = 0;
+    private int writeRequests = 0;
+    private Thread writingThread = null;
+
+    @Override
+    public synchronized void lockWrite() {
+        writeRequests++;
+        Thread callingThread = Thread.currentThread();
+        while (! canGrantWriteAccess(callingThread)) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                // ...
+            }
+        }
+        writeRequests--;
+        writeAccesses++;
+        writingThread = callingThread;
+    }
+
+    @Override
+    public synchronized void unlockWrite() {
+        writeAccesses--;
+        if (writeAccesses == 0) {
+            writingThread = null;
+        }
+        notifyAll();
+    }
+
+    private boolean canGrantWriteAccess(Thread thread) {
+        if (hasReaders()) return false;
+        if (writingThread == null) return true;
+        if (!isWriter(thread)) return false;
+        return true;
+    }
+
+    private boolean hasReaders(){
+        return readingThreads.size() > 0;
+    }
+
+    private boolean isWriter(Thread thread){
+        return writingThread == thread;
+    }
+
+    @Override
+    public void lockRead() {}
+
+    @Override
+    public void unlockRead() {}
+}
