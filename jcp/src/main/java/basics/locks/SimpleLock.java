0635c9b17862552eb53c4479f8c9f4bbcb1fbe4a
Artur Khalikov <akhalikoff@gmail.com>
2015-10-12 13:57:21 +0300

    Update SimpleLock.java

--- jcp/src/main/java/basics/locks/SimpleLock.java
+++ jcp/src/main/java/basics/locks/SimpleLock.java
@@ -1,24 +1,28 @@
 package basics.locks;
 
 /**
- * See {@link http://tutorials.jenkov.com/java-concurrency/locks.html}
- *
  * Simple lock implementation
  * Not-reentrant, not-fair
+ *
+ * {@see http://tutorials.jenkov.com/java-concurrency/locks.html}
  */
 class SimpleLock {
 
 	private boolean isLocked = false;
 
-  	public synchronized void lock() throws InterruptedException {
-    	while (isLocked) {
-      		wait();
-    	}
-    	isLocked = true;
-  	}
+	public synchronized void lock() {
+		while (isLocked) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// ...
+			}
+		}
+		isLocked = true;
+	}
 
-  	public synchronized void unlock() {
-    	isLocked = false;
-    	notify();
-  	}
+	public synchronized void unlock() {
+		isLocked = false;
+		notify();
+	}
 }
