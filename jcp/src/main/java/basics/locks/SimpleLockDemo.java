edf71afec7bb2e5a5df56608629f496ed435cd9d
Artur Khalikov <akhalikoff@gmail.com>
2015-10-12 15:06:59 +0300

    Create SimpleLockDemo.java

---
+++ jcp/src/main/java/basics/locks/SimpleLockDemo.java
@@ -0,0 +1,33 @@
+package basics.locks;
+
+/**
+ * Example of using simple lock
+ */
+public class SimpleLockDemo {
+
+    static class Counter {
+        private SimpleLock lock = new SimpleLock();
+        private int count = 0;
+
+        public int increment() {
+            lock.lock();
+            int newCount = ++count;
+            lock.unlock();
+            return newCount;
+        }
+    }
+
+    private static Counter counter = new Counter();
+
+    public static void main(String[] args) {
+        Thread[] threads = new Thread[2];
+        for (int i = 0; i < threads.length; i++) {
+            (threads[i] = new Thread(() -> {
+                int val;
+                do {
+                    val = counter.increment();
+                } while (val <= 1_000_000);
+            })).start();
+        }
+    }
+}
