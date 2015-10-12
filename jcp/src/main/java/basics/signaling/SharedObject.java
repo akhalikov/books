f482b88e3347a2deedbe71a991c59737eec1d1b2
Artur Khalikov <akhalikoff@gmail.com>
2015-10-12 14:57:40 +0300

    Update SharedObject.java

--- jcp/src/main/java/basics/signaling/SharedObject.java
+++ jcp/src/main/java/basics/signaling/SharedObject.java
@@ -1,17 +1,15 @@
 package basics.signaling;
 
 /**
- * See {@link http://tutorials.jenkov.com/java-concurrency/thread-signaling.html}
- *
+ * A simple way to send signals via shared objects.
+ * {@See http://tutorials.jenkov.com/java-concurrency/thread-signaling.html}
  */
 class SharedObject {
-
 	/**
-	 * A simple way to send signals via shared objects.
-	 * Thread A and B must have a reference to a shared MySignal instance for the signaling to work. 
+	 * Thread A and B must have a reference to a shared MySignal instance for the signaling to work.
 	 */
 	static class MySignal {
-		protected boolean hasDataToProcess = false;
+        protected boolean hasDataToProcess = false;
 
 		public synchronized boolean hasDataToProcess() {
 			return hasDataToProcess;
