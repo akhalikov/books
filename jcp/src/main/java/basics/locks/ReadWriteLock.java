b473bd26f7c39fd8527ff85e0e51ddf570b831bd
Artur Khalikov <akhalikoff@gmail.com>
2015-10-13 15:16:46 +0300

    ReadWriteLock interface added

---
+++ jcp/src/main/java/basics/locks/ReadWriteLock.java
@@ -0,0 +1,17 @@
+package basics.locks;
+
+/**
+ * Read write lock interface
+ *
+ * @author ahalikov
+ */
+public interface ReadWriteLock {
+
+    void lockRead();
+
+    void unlockRead();
+
+    void lockWrite();
+
+    void unlockWrite();
+}
