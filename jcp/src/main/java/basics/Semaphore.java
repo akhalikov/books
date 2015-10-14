362ce7652847c0b3b50dd26ba6696a841fee24f4
Artur Khalikov <akhalikoff@gmail.com>
2015-10-14 16:56:56 +0300

    Create Semaphore.java

---
+++ jcp/src/main/java/basics/Semaphore.java
@@ -0,0 +1,21 @@
+package basics;
+
+/**
+ * Simple semaphore implementation
+ */
+public class Semaphore {
+
+    private boolean signal = false;
+
+    public synchronized void take() {
+        this.signal = true;
+        this.notify();
+    }
+
+    public synchronized void release() throws InterruptedException {
+        while (!this.signal) {
+            wait();
+        }
+        this.signal = false;
+    }
+}
