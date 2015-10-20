788bb9c90b2fa5dab0b667549fa3bbef8fe20221
Artur Khalikov <akhalikoff@gmail.com>
2015-10-20 23:47:48 +0300

    common utils

---
+++ jcp/src/main/java/common/LaunderThrowable.java
@@ -0,0 +1,22 @@
+package common;
+
+/**
+ * LaunderThrowable class
+ *
+ * Coerce an unchecked Throwable to a RuntimeException
+ * If the Throwable is an Error, throw it;
+ * If it is a RuntimeException return it, otherwise throw IllegalStateException
+ */
+public class LaunderThrowable {
+
+    public static RuntimeException launderThrowable(Throwable t) {
+        if (t instanceof RuntimeException)
+            return (RuntimeException) t;
+
+        else if (t instanceof Error)
+            throw (Error) t;
+
+        else
+            throw new IllegalStateException("Not unchecked", t);
+    }
+}
