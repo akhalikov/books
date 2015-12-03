2868708b7bded64b1e3233858c09da2aa46fab0e
Artur Khalikov <akhalikoff@gmail.com>
2015-12-03 20:23:11 +0300

    java8 for impatient: chapter 2

---
+++ java8-for-impatient/src/main/java/chapter2/Stateful.java
@@ -0,0 +1,24 @@
+package chapter2;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+/**
+ * Stateful transformations
+ *
+ * @author ahalikov
+ */
+public class Stateful {
+
+  public static void main(String[] args) {
+    Stream<String> uniqueWords
+      = Stream.of("merrily", "merrily", "merrily", "gently").distinct();
+    uniqueWords.forEach(s -> System.out.println(s));
+
+    Stream<String> words = Arrays.asList("Maybe", "Leonardo", "Compensation").stream();
+    Stream<String> longestFirst =
+      words.sorted(Comparator.comparing(String::length).reversed());
+    longestFirst.forEach(System.out::println);
+  }
+}
