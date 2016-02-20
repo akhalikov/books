22b35064a13d8e7efe786b1bd495df71c70e0820
Artur Khalikov <akhalikoff@gmail.com>
2016-02-20 23:44:07 +0300

    Triple step

---
+++ cracking-the-coding-interview/src/main/java/careercup/chapter08/TripleStep.java
@@ -0,0 +1,15 @@
+package careercup.chapter08;
+
+/**
+ * A child is running up a staircase with n steps and can hop
+ * either 1 step, 2 steps, or 3 steps at a time.
+ * Implement a method to count how many possible ways the child
+ * can run up the stairs.
+ *
+ * Created by akhalikov on 20/02/16
+ */
+public class TripleStep {
+  static int countTripleSteps() {
+    return 0;
+  }
+}
---
+++ cracking-the-coding-interview/src/test/java/careercup/chapter08/TripleStepTest.java
@@ -0,0 +1,15 @@
+package careercup.chapter08;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test for {@link TripleStep}
+ */
+public class TripleStepTest {
+  @Test
+  public void testCountTripleSteps() {
+
+  }
+}
