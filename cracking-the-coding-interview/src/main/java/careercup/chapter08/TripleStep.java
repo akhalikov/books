b142347fea71b1d77c6b95d64fe9c3f420899bed
Artur Khalikov <akhalikoff@gmail.com>
2016-02-21 01:41:04 +0300

    Triple step

--- cracking-the-coding-interview/src/main/java/careercup/chapter08/TripleStep.java
+++ cracking-the-coding-interview/src/main/java/careercup/chapter08/TripleStep.java
@@ -1,5 +1,7 @@
 package careercup.chapter08;
 
+import java.util.Arrays;
+
 /**
  * A child is running up a staircase with n steps and can hop
  * either 1 step, 2 steps, or 3 steps at a time.
@@ -9,7 +11,68 @@ package careercup.chapter08;
  * Created by akhalikov on 20/02/16
  */
 public class TripleStep {
-  static int countTripleSteps() {
-    return 0;
+  /**
+   * Brute-force solution, running for O(3^n)
+   */
+  static int countWays(int n) {
+    if (n < 0)
+      return 0;
+    else if (n == 0)
+      return 1;
+    else
+      return countWays(n - 1) + countWays(n - 2) + countWays(n - 3);
+  }
+
+  /**
+   * Iterative solution, running for O(n)
+   * Space complexity: O(1)
+   *
+   * n = 0: prev1 = prev2 = prev3 = 0
+   * n = 1: prev1 = 1, prev2 = 0, prev3 = 0
+   * n = 2: prev1 = 1, prev2 = 1, prev3 = 0
+   */
+  static int countWaysIter(int n) {
+
+    if (n == 0)
+      return 0;
+    else if (n == 1)
+      return 1;
+    else {
+      int prev1 = 1, prev2 = 1, prev3 = 0;
+      int count = 0;
+      for (int i = 2; i <= n; i++) {
+        count = prev1 + prev2 + prev3;
+        prev3 = prev2;
+        prev2 = prev1;
+        prev1 = count;
+      }
+      return count;
+    }
+  }
+
+  /**
+   * Memoization solution
+   */
+  static int countWaysMemo(int n) {
+    int[] memo = new int[n + 1];
+    Arrays.fill(memo, -1);
+    return countWaysMemo(n, memo);
+  }
+
+  static int countWaysMemo(int n, int[] memo) {
+    if (n < 0)
+      return 0;
+    else if (n == 0)
+      return 1;
+    else if (memo[n] > -1) {
+      return memo[n];
+    } else {
+      memo[n] =
+        countWaysMemo(n-1, memo)
+        + countWaysMemo(n-2, memo)
+        + countWaysMemo(n-3, memo);
+
+      return memo[n];
+    }
   }
 }
--- cracking-the-coding-interview/src/test/java/careercup/chapter08/TripleStepTest.java
+++ cracking-the-coding-interview/src/test/java/careercup/chapter08/TripleStepTest.java
@@ -9,7 +9,33 @@ import static org.junit.Assert.*;
  */
 public class TripleStepTest {
   @Test
-  public void testCountTripleSteps() {
+  public void testCountWays() {
+    assertEquals(1, TripleStep.countWays(1));
+    assertEquals(2, TripleStep.countWays(2));
+    assertEquals(4, TripleStep.countWays(3));
+  }
 
+  @Test
+  public void testCountWaysIter() {
+    assertEquals(1, TripleStep.countWaysIter(1));
+    assertEquals(2, TripleStep.countWaysIter(2));
+    assertEquals(4, TripleStep.countWaysIter(3));
+
+    int expected = TripleStep.countWays(7);
+    System.out.println("countWays(7)=" + expected);
+    assertEquals(expected, TripleStep.countWaysIter(7));
+
+    expected = TripleStep.countWays(12);
+    System.out.println("countWays(12)=" + expected);
+    assertEquals(expected, TripleStep.countWaysIter(12));
+  }
+
+  @Test
+  public void testCountWaysMemo() {
+    assertEquals(1, TripleStep.countWaysMemo(1));
+    assertEquals(2, TripleStep.countWaysMemo(2));
+    assertEquals(4, TripleStep.countWaysMemo(3));
+    assertEquals(44, TripleStep.countWaysMemo(7));
+    assertEquals(927, TripleStep.countWaysMemo(12));
   }
 }
