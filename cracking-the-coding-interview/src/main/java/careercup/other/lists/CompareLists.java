dc342758a84cfdc03e04fd256de296bf5653cf6f
Artur Khalikov <akhalikoff@gmail.com>
2016-02-27 09:26:50 +0200

    Lists compare

--- cracking-the-coding-interview/src/main/java/careercup/other/lists/CompareLists.java
+++ cracking-the-coding-interview/src/main/java/careercup/other/lists/CompareLists.java
@@ -26,4 +26,46 @@ package careercup.other.lists;
  * Created by akhalikov on 27/02/16
  */
 public class CompareLists {
+
+  static int compare(Node list1, Node list2) {
+    // run through lists
+    while (list1 != null
+        && list2 != null
+        && list1.value == list2.value) {
+      list1 = list1.next;
+      list2 = list2.next;
+    }
+
+    // if we stopped, it means that either one or both of the lists are null,
+    // or characters are not equal
+
+    // if list1 is shorter than list2 => list1 is less than list2
+    if (list1 == null && list2 != null)
+      return -1;
+
+    // if list2 is shorter than list1 => list1 is greater than list2
+    if (list1 != null && list2 == null)
+      return 1;
+
+    // if characters are not equal
+    if (list1 != null && list2 != null)
+      return list1.value > list2.value ? 1: -1;
+
+    // lists are equal
+    return 0;
+  }
+
+  static class Node {
+    char value;
+    Node next;
+
+    public Node(char c) {
+      this.value = c;
+    }
+
+    public Node(char c, Node next) {
+      this.value = c;
+      this.next = next;
+    }
+  }
 }
