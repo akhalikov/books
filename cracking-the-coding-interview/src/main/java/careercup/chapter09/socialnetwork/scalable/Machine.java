860d300ecf705676d47a3f45e7d8a040ef4ac592
Artur Khalikov <akhalikoff@gmail.com>
2016-03-02 21:25:56 -0800

    Social Network

---
+++ cracking-the-coding-interview/src/main/java/careercup/chapter09/socialnetwork/scalable/Machine.java
@@ -0,0 +1,10 @@
+package careercup.chapter09.socialnetwork.scalable;
+
+import careercup.chapter09.socialnetwork.Person;
+
+/**
+ * Created by akhalikov on 03/03/16
+ */
+public interface Machine {
+  Person getPersonWithId(int personid);
+}
---
+++ cracking-the-coding-interview/src/main/java/careercup/chapter09/socialnetwork/scalable/Server.java
@@ -0,0 +1,35 @@
+package careercup.chapter09.socialnetwork.scalable;
+
+import careercup.chapter09.socialnetwork.Person;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Class Server holds a list of all the machines on which we store Persons
+ *
+ * Created by akhalikov on 03/03/16
+ */
+public class Server {
+  private Map<Integer, Machine> machines = new HashMap<>();
+  private Map<Integer, Integer> personToMachineMap = new HashMap<>();
+
+  public Machine getMachineWithId(int machineId) {
+    return machines.get(machineId);
+  }
+
+  public int getMachineIdForuser(int personId) {
+    Integer machineId = personToMachineMap.get(personId);
+    return machineId == null ? -1: machineId;
+  }
+
+  public Person getPersonWithId(int personId) {
+    Integer machineId = personToMachineMap.get(personId);
+    if (machineId == null) return null;
+
+    Machine machine = getMachineWithId(machineId);
+    if (machine == null) return null;
+
+    return machine.getPersonWithId(personId);
+  }
+}
