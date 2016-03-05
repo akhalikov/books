b51c0d7f63c6c3eb02bc8e5a4ff08afc52a39d95
Artur Khalikov <akhalikoff@gmail.com>
2016-03-04 17:36:51 -0800

    Common elements

---
+++ cracking-the-coding-interview/src/main/java/other/arrays/CommonElements.java
@@ -0,0 +1,18 @@
+package other.arrays;
+
+/**
+ * Find common elements from n sorted arrays
+ *
+ * 2 3 5 6 10
+ * 0 1 3 5 9 10 19
+ * 5 11 19
+ *
+ * Created by akhalikov on 03/03/16
+ */
+class CommonElements {
+  static void printCommonElements(int[][] arrays) {
+    if (arrays == null)
+      throw new NullPointerException("arrays are null");
+
+  }
+}
