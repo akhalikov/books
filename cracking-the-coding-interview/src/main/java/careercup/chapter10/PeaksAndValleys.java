5f66538676b97a47a1d1facf0f7aa81318bd4497
Artur Khalikov <akhalikoff@gmail.com>
2016-03-05 20:38:58 -0800

    Peaks and valleys

--- cracking-the-coding-interview/src/main/java/careercup/chapter10/PeaksAndValleys.java
+++ cracking-the-coding-interview/src/main/java/careercup/chapter10/PeaksAndValleys.java
@@ -11,4 +11,33 @@ package careercup.chapter10;
  * Created by artur on 20.01.16.
  */
 public class PeaksAndValleys {
+  static void sortPeakValleys(int[] array) {
+    if (array == null)
+      throw new NullPointerException("array is null");
+    if (array.length < 3)
+      return;
+
+    for (int i = 1; i < array.length; i+=2) {
+      int maxIndex = getMaxIndex(array, i-1, i, i+1);
+      if (i != maxIndex)
+        swap(array, i, maxIndex);
+    }
+  }
+
+  private static int getMaxIndex(int[] a, int lo, int mid, int hi) {
+    int loVal = (lo >= 0 && lo < a.length) ? a[lo]: Integer.MIN_VALUE;
+    int midVal = (mid >= 0 && mid < a.length) ? a[mid]: Integer.MIN_VALUE;
+    int hiVal = (hi >= 0 && hi < a.length) ? a[hi]: Integer.MIN_VALUE;
+
+    int maxVal = Math.max(loVal, Math.max(midVal, hiVal));
+    if (maxVal == loVal) return lo;
+    else if (maxVal == midVal) return mid;
+    else return hi;
+  }
+
+  private static void swap(int[] a, int i, int j) {
+    int temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
+  }
 }
