67d469ac4c9805cd9b8d5f9a407b2507f96b314b
Artur Khalikov <akhalikoff@gmail.com>
2016-03-07 23:17:44 -0800

    Remove "other" folder

---
+++ cracking-the-coding-interview/src/test/java/careercup/chapter08/PaintFillTest.java
@@ -0,0 +1,16 @@
+package careercup.chapter08;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test for {@link PaintFill}
+ */
+public class PaintFillTest {
+
+  @Test
+  public void testPaintFill() {
+
+  }
+}
